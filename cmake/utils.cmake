4b6f6f5bd0e354d0053fcdd213df40c8edb92996
Ashwin Natesan <ashwin.natesan@ittiam.com>
2023-03-28 18:07:00 +0530

    Added support for MacOS builds

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -17,6 +17,12 @@ function(defineInterfaces)
     target_compile_options("${projName}CompileOptions"
                            INTERFACE -Wall -Wextra -Wpedantic -Werror)
 
+    if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
+      target_compile_definitions("${projName}CompileOptions" INTERFACE MACOS)
+    else()
+      target_compile_definitions("${projName}CompileOptions" INTERFACE LINUX)
+    endif()
+
     if(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
       target_compile_options("${projName}CompileOptions"
                              INTERFACE -march=armv8-a)
