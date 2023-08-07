ac09f8e198aad57e6b17d902e070cf9d0f79cd0c
100863 <aryan.kulshreshtha@ittiam.com>
2023-08-07 14:54:27 +0530

    Include paths cleanup

--- plugin/browser_plugin/fgs-module.c
+++ plugin/browser_plugin/fgs-module.c
@@ -44,15 +44,11 @@
 */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdint.h>
-#include <stdarg.h>
-#include <math.h>
 #include <stdlib.h>
-#include <assert.h>
 
-#include "../../fgs/api/error_codes.h"
-#include "../../fgs/api/fgs_api.h"
+#include "error_codes.h"
+#include "fgs_api.h"
 #include "fgs-module-priv.h"
 
 GrainCharacteristicApi fgcCTx     = { 0 };
